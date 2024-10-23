50135e21ba68ef4cd2c4533b73f2f938478cc75c
Alperen Güneş <alperenmanavgat@outlook.com>
2024-10-23 15:06:27 +0300

    Exception handling has been added.

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,54 @@
+﻿int a = 1;
+int b = 0;
+
+//try
+//{
+//    Console.WriteLine(a / b);
+//}
+
+//catch (Exception ex)
+//{
+
+//}
+
+try
+{
+    while (true)
+    {
+        var key = Console.ReadKey();
+        if (key.Key == ConsoleKey.Z)
+        {
+            throw new CustomException("Dont type Z")
+            {
+                MyProperty = "My custom property"
+            };
+        }
+
+        else
+        {
+            Console.WriteLine("You typed: " + key.KeyChar);
+        }
+    }
+}
+catch (CustomException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+class CustomException : Exception
+{
+    public string MyProperty { get; set; }
+    public CustomException(): base("Custom Exception")
+    {
+
+    }
+    public CustomException(string message): base (message)
+    {
+
+    }
+
+
+}
