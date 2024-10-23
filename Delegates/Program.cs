e1733d3e992569ea5e5d59f676c1f5105fd8cde6
Alperen Güneş <alperenmanavgat@outlook.com>
2024-10-23 22:25:47 +0300

    multicast delegate

--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -1,40 +1,64 @@
-﻿XHandler xdelegate= new XHandler(X);
-XHandler xdelegate2 = X;
-XHandler xdelegate3 = () => { };
-XHandler xdelegate4 = delegate (){ };
+﻿//XHandler xdelegate= new XHandler(X);
+//XHandler xdelegate2 = X;
+//XHandler xdelegate3 = () => { };
+//XHandler xdelegate4 = delegate (){ };
 
-xdelegate();
-xdelegate.Invoke();
+//xdelegate();
+//xdelegate.Invoke();
 
-YHandler ydelegate = new YHandler(Y);
-YHandler ydelegate2 = Y;
-YHandler ydelegate3 = (A a, (string, int) p) => (1, 'a');
-//YHandler ydelegate3=(a,p)=>
+//YHandler ydelegate = new YHandler(Y);
+//YHandler ydelegate2 = Y;
+//YHandler ydelegate3 = (A a, (string, int) p) => (1, 'a');
+////YHandler ydelegate3=(a,p)=>
+////{
+////    return (1, 'a');
+////};
+//YHandler ydelegate4 = delegate (A a, (string, int) p) 
+//{ 
+//    return (1, 'a'); 
+//};
+
+//(int, char) r = ydelegate4(new(), ("alperen", 24));
+//var r2 = ydelegate4.Invoke(new(), ("alperen", 24));
+
+//void X()
+//{
+//    Console.WriteLine("X-method");
+//}
+
+//(int,char)Y(A a, (string, int) p)
 //{
 //    return (1, 'a');
-//};
-YHandler ydelegate4 = delegate (A a, (string, int) p) 
-{ 
-    return (1, 'a'); 
-};
+//}
 
-(int, char) r = ydelegate4(new(), ("alperen", 24));
-var r2 = ydelegate4.Invoke(new(), ("alperen", 24));
+//public delegate void XHandler();
 
-void X()
+//public delegate (int, char) YHandler(A a,(string,int)p);
+
+
+XHandler xdelegate = () => Console.WriteLine("Method1");
+xdelegate += () => Console.WriteLine("Method2");
+xdelegate += Method3;
+xdelegate += () => Console.WriteLine("Method4");
+xdelegate += () => Console.WriteLine("Method5");
+
+xdelegate();
+xdelegate -= Method3;
+Console.WriteLine("***");
+xdelegate();
+
+var delegates = xdelegate.GetInvocationList();
+foreach (var item in delegates)
 {
-    Console.WriteLine("X-method");
+    Console.WriteLine(item.Method.ToString());
 }
 
-(int,char)Y(A a, (string, int) p)
+void Method3()
 {
-    return (1, 'a');
+    Console.WriteLine("Method3");
 }
 
 public delegate void XHandler();
-
-public delegate (int, char) YHandler(A a,(string,int)p);
-
 public class A
 {
    
