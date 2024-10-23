77493a31d2fded3cfb908b0a10eac41585b355d8
Alperen Güneş <alperenmanavgat@outlook.com>
2024-10-23 23:09:42 +0300

    Events

---
+++ Events/Program.cs
@@ -0,0 +1,62 @@
+﻿string path= @"C:\Users\alper\Desktop\ıvır zıvır";
+PathControl pathControl = new();
+pathControl.PathControlEvent += (sizeMB) => Console.WriteLine($"Path has been exceeded 50MB Last Size->{sizeMB}");
+await pathControl.Control(path);
+
+class PathControl
+{
+    public delegate void PathHandler(float sizeMB);
+    public event PathHandler PathControlEvent;
+
+    public async Task Control(string path)
+    {
+        while (true)
+        {
+            await Task.Delay(1000);
+            DirectoryInfo directoryInfo = new(path);
+            var files = directoryInfo.GetFiles();
+            float size= await Task.Run(() => directoryInfo.EnumerateFiles("*", SearchOption.AllDirectories).Sum(file => file.Length));
+            float sizeMB = (size / 1024) / 1024;
+            if (sizeMB > 100)
+            {
+                PathControlEvent?.Invoke(sizeMB);
+            }
+        }
+    }
+}
+
+//MyEventPublisher p = new();
+//MyEventPublisher.XHandler xdelegate= new MyEventPublisher.XHandler(X);
+//p.MyEvent += X;
+
+//p.RaiseEvent();
+//p.MyEvent -= X;
+//void X()
+//{
+//    Console.WriteLine("Event has been triggered");
+//}
+//class MyEventPublisher
+//{
+//    public delegate void XHandler();
+//    XHandler xdelegate;
+
+//    public event XHandler MyEvent
+//    {
+//        add
+//        {
+//            Console.WriteLine("Event has been added");
+//            xdelegate += value;
+//        }
+//        remove
+//        {
+//            Console.WriteLine("Event has been removed");
+//            xdelegate -= value;
+//        }
+//    }
+
+//    public void RaiseEvent()
+//    {
+//        //MyEvent?.Invoke();
+//        xdelegate?.Invoke();
+//    }
+//}
