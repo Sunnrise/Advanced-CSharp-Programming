5765cf2dbb66c0d209d4112db2de703d96ea93c4
Alperen Güneş <alperenmanavgat@outlook.com>
2024-10-23 14:28:56 +0300

    Implicit and Explicit overloading

---
+++ Implicit_and_ExplicitOperatorOverloading/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine();
+A a1 = new B(); // Implicit conversion
+A a2 = (A)new B(); // Explicit conversion
+
+// Implicit conversion
+B b = (B)new A();
+class A 
+{
+    public static explicit operator B(A i)
+    {
+        return new B();
+    }
+
+}
+class B
+{
+    public static implicit operator A(B i)
+    {
+        return new A() { };
+    }
+}
