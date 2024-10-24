cad6854e18e2568df5162296bfe68dbc525beba2
Alperen Güneş <alperenmanavgat@outlook.com>
2024-10-24 11:41:42 +0300

    Access modifiers

---
+++ AccessModifiers/AccessModifiersClassLibrary/Class1.cs
@@ -0,0 +1,33 @@
+﻿namespace AccessModifiersClassLibrary
+{
+    public class Class1
+    {
+        public void X() 
+        {
+            Y(); // We can call Y() from X() because they are in the same class
+        }
+        void Y() 
+        {
+            Z(); // We can call Z() from Y() because they are in the same class
+        } // Y() is private by default
+
+        protected void Z() { } // Z() is protected
+
+        internal void W() { } // W() is internal
+        protected internal void V() { } // V() is protected internal
+        private protected void U() { } // U() is private protected
+    }
+    class Class2 : Class1
+    {
+        public Class2()
+        {
+            Class1 c1 = new();
+            c1.Z(); // Error: 'Class1.Z()' is inaccessible due to its protection level(protected)
+            Z(); // We can call Z() from Class2 because Class2 is a subclass of Class1
+            c1.W(); // We can call W() from Class2 because W() is internal
+            c1.V(); // We can call V() from Class2 because V() is protected internal
+            c1.U(); // Error: 'Class1.U()' is inaccessible due to its protection level(private 'protected')
+            U(); // We can call U() from Class2 because Class2 is a subclass of Class1 (U() is private 'protected')
+        }
+    }
+}
---
+++ AccessModifiers/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AccessModifiersClassLibrary;
+
+Console.WriteLine("Hello, World!");
+
+Class1 c1 = new();
+
+c1.X();
+c1.Y(); // Error: 'Class1.Y()' is inaccessible due to its protection level(private)
+c1.Z(); // Error: 'Class1.Z()' is inaccessible due to its protection level(protected)
+
+Class2 c2 = new(); // Error: 'Class2' is inaccessible due to its protection level(private)
+
+c1.V(); // Error: 'Class1.V()' is inaccessible due to its protection level(protected internal)
+
+class MyClass: Class1
+{
+    public MyClass()
+    {
+        V(); // We can call V() from MyClass because MyClass is a subclass of Class1
+        U(); // Error: 'Class1.U()' is inaccessible due to its protection level('private' protected)
+    }
+}
