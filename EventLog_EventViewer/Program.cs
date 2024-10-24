d9a57c8fd2ef27906128469d32d5fd4a3d8bf1a6
Alperen Güneş <alperenmanavgat@outlook.com>
2024-10-24 22:39:37 +0300

    EventLog

---
+++ EventLog_EventViewer/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+
+const string eventLogSource = "MyApplicationExample";
+
+string logName = "MyApplicationLog";
+
+if (!EventLog.SourceExists(eventLogSource))
+{
+    EventLog.CreateEventSource(eventLogSource, logName);
+    Console.WriteLine($"Event Log has been created : {logName}");
+}
+string logMessage = $"Event Log Message: {DateTime.Now}";
+Console.WriteLine(logMessage);
+EventLog.WriteEntry(eventLogSource, logMessage, EventLogEntryType.Information);
+Console.WriteLine("Log has been written");
+Process.Start("eventvwr.exe");
